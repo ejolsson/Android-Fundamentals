033a38e638fffe6c3e74d5c440a45959337ad10a
Eric Olsson <ejolsson@gmail.com>
2023-04-24 15:50:16 +0200

    Fight page added. Buttons a bit wide for narrow screens.

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+//        setContentView(R.layout.login)
+//        setContentView(R.layout.hero_list)
+        setContentView(R.layout.hero_fight)
     }
 }
